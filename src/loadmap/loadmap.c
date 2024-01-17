f4d30691be2a7237db973caf1fbef5e0968e8803
jonaspeters85 <jonaspeters8526@gmail.com>
2024-01-17 20:30:44 +0100

    gnl running

--- src/loadmap/loadmap.c
+++ src/loadmap/loadmap.c
@@ -5,21 +5,30 @@
 #include "../../libs/libft/include/libft.h"
 #include "../../libs/libft/include/get_next_line.h"
 
-int get_line_count(int fd)
+int get_line_count(char *file)
 {
+    int fd;
     int count = 0;
     char buf;
     int re = 1;
 
+    fd = open(file, O_RDONLY);
+    if (fd == -1)
+    {
+        printf("Error\nfile loading\n");
+        // exit
+        return (-1);
+    }
     while(re > 0)
     {
         re = read(fd, &buf, 1);
         if (buf == '\n')
             count++;
+        //printf("%c", buf);
     }
     if (count > 0)
         count++;
-    
+    close(fd);
     //printf("get line count: %i\n", count);
     return (count);
 }
@@ -28,7 +37,7 @@ void show_arr(char **arr)
 {
     int i = -1;
     printf("show array \n");
-    while(arr[i++])
+    while(i++, arr[i])
         printf("i: %i  >%s<\n", i, arr[i]);
 }
 
@@ -45,31 +54,32 @@ char **J_load_file(char *file)
         printf("Error\nfile loading\n");
         // exit
     }
-    printf("fd: %i \n",fd);
-    line_count = get_line_count(fd);
+    line_count = get_line_count(file);
+    printf("nach linecount fd: %i    linecount: %i\n",fd, line_count);
+
     if (line_count < 8)
     {
         printf("Error\nnot enought information in map\n");
     }
     tmp =(char **)malloc((line_count + 1) * sizeof(char *));
     i = 0;
+    char *test;
     while(i < line_count)
     {
-        printf("fd: %i\n");
+        printf("fd: %i\n", fd);
         tmp[i] = get_next_line(fd);
         printf("line: %i  >%s<\n", i, tmp[i]);
         if (tmp[i] == NULL)
             break;
         i++;
     }
+    tmp[i] = NULL;
     show_arr(tmp);
     //tmp = get_next_line(fd);
     
     close(fd);
     return (NULL);
 }
-
-
 
 int main(int argc, char **argv)
 {   
@@ -80,5 +90,5 @@ int main(int argc, char **argv)
 }
 
 /*
-cc loadmap.c ../../libs/libft/libft.a && ./a.out map1.cub
+cc -D BUFFER_SIZE=1 loadmap.c ../../libs/libft/libft.a && ./a.out map1.cub
 */ 
