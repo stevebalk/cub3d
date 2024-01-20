a0abfd0189cdce994d1a827145ba403a5a2d705c
stevebalk <steve.balk@web.de>
2024-01-20 01:18:21 +0100

    Added basic wall collision

--- src/player/movement.c
+++ src/player/movement.c
@@ -6,60 +6,51 @@
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 13:18:44 by sbalk             #+#    #+#             */
-/*   Updated: 2024/01/20 00:05:18 by sbalk            ###   ########.fr       */
+/*   Updated: 2024/01/20 01:15:59 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// int	can_move(t_cub *cub, t_vec2 dir)
-// {
-// 	t_vec2i	new_pos;
+int	collidesWithWall(t_cub *cub, t_vec2 position)
+{
+	int mapX = (int)(position.x);
+	int mapY = (int)(position.y);
 
-// 	new_pos.x = round(cub->player.pos.x + dir.x * MOVE_SPEED);
-// 	new_pos.y = round(cub->player.pos.y + dir.y * MOVE_SPEED);
-// 	printf("New pos: %d, %d\n", new_pos.x, new_pos.y);
-// 	if (cub->map[new_pos.y][new_pos.x] >= 1)
-// 		return (0);n_move(t_cub *cub, t_vec2 dir)
-// {
-// 	t_vec2i	new_pos;
-
-// 	new_pos.x = round(cub->player.pos.x + dir.x * MOVE_SPEED);
-// 	new_pos.y = round(cub->player.pos.y + dir.y * MOVE_SPEED);
-// 	printf("New pos: %d, %d\n", new_pos.x, new_pos.y);
-// 	if (cub->map[new_pos.y][new_pos.x] >= 1)
-// 		return (0);
-// 	return (1);
-// }
-// 	return (1);
-// }
+	if (cub->map[mapY][mapX] >= 1)
+		return (1);
+	return (0);
+}
 
 void	move(t_cub *cub)
 {
 	t_vec2	new_pos;
 
-	new_pos.x = cub->player.pos.x + cub->player.velocity.x;
-	new_pos.y = cub->player.pos.y + cub->player.velocity.y;
-	cub->player.pos.x = new_pos.x;
-	cub->player.pos.y = new_pos.y;
-	// if (cub->map[(int)round(new_pos.y)][(int)round(cub->player.pos.x)] == 0)
-	// 	cub->player.pos.x = new_pos.x;
-	// if (cub->map[(int)round(cub->player.pos.y)][(int)round(new_pos.x)] == 0)
-	// 	cub->player.pos.y = new_pos.y;
+	new_pos.x = cub->player.pos.x + cub->player.velocity.x * MOVE_SPEED;
+	new_pos.y = cub->player.pos.y + cub->player.velocity.y * MOVE_SPEED;
+	if (new_pos.x >= 0 && new_pos.x < cub->map_size.x * TILE_SIZE &&
+		new_pos.y >= 0 && new_pos.y < cub->map_size.y * TILE_SIZE)
+	{
+		if (!collidesWithWall(cub, new_pos))
+		{
+			cub->player.pos.x = new_pos.x;
+			cub->player.pos.y = new_pos.y;
+		}
+	}
 	cub->player.velocity.x = 0;
 	cub->player.velocity.y = 0;
 }
 
 void	move_forward(t_cub *cub)
 {
-	cub->player.velocity.x += cub->player.dir.x * MOVE_SPEED;
-	cub->player.velocity.y += cub->player.dir.y * MOVE_SPEED;
+	cub->player.velocity.x += cub->player.dir.x;
+	cub->player.velocity.y += cub->player.dir.y;
 }
 
 void	move_backward(t_cub *cub)
 {
-	cub->player.velocity.x -= cub->player.dir.x * MOVE_SPEED;
-	cub->player.velocity.y -= cub->player.dir.y * MOVE_SPEED;
+	cub->player.velocity.x -= cub->player.dir.x;
+	cub->player.velocity.y -= cub->player.dir.y;
 }
 
 void	move_left(t_cub *cub)
@@ -68,8 +59,8 @@ void	move_left(t_cub *cub)
 
 	new_dir.x = cub->player.dir.y;
 	new_dir.y = -cub->player.dir.x;
-	cub->player.velocity.x += new_dir.x * MOVE_SPEED;
-	cub->player.velocity.y += new_dir.y * MOVE_SPEED;
+	cub->player.velocity.x += new_dir.x;
+	cub->player.velocity.y += new_dir.y;
 }
 
 void	move_right(t_cub *cub)
@@ -78,6 +69,6 @@ void	move_right(t_cub *cub)
 
 	new_dir.x = -cub->player.dir.y;
 	new_dir.y = cub->player.dir.x;
-	cub->player.velocity.x += new_dir.x * MOVE_SPEED;
-	cub->player.velocity.y += new_dir.y * MOVE_SPEED;
+	cub->player.velocity.x += new_dir.x;
+	cub->player.velocity.y += new_dir.y;
 }
