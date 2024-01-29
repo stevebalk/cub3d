7372b5d108feaf801fd87ede29b90fb9d3dec05e
Jonas Peters <jopeters@c2s17d100.42wolfsburg.de>
2024-01-29 14:17:41 +0100

    flood working, but whitout arror handling

--- src/loadmap/levelcheck.c
+++ src/loadmap/levelcheck.c
@@ -4,7 +4,7 @@
 Flood simulation.
 
 1) copy map   	✓
-2) setting startpoint to 'f'
+2) setting startpoint to 'f' 	✓
 3) Check near fields
 	a) 4 Check functions   up, right, down, left	
 	b) if true --> write 'F' in array
@@ -23,15 +23,126 @@ void set_first_f(char **arr)
 	replace_char_in_arr(arr, 'W', 'F');
 }
 
+int check_field(char **arr, int x, int y)
+{
+	if (arr[y][x] == '0')
+	{
+		replace_char_at_pos(arr, y, x, 'F');
+		usleep(50000);
+		clear_screen();
+		show_map(arr);
+	}
+	else if (arr[y][x] == ' ')
+	{
+		c_red();
+		printf("Error!\nspace in map!\n"); c_reset();
+		return (0);
+	}
+	else
+	{
+
+	}
+
+	return (1);
+}
+
+// checks all reachable fields of the acutal position and fills with F if possible
+int check_near_fields(char **arr, int x, int y)
+{
+	int arr_len;
+
+	arr_len = get_arr_len(arr);
+	//printf("check_near_fields  x: %i   y: %i \n", x, y);
+	//printf("arr len: %i\n", arr_len);
+	// UP
+	if (y - 1 > 0)
+	{
+		if (x < ft_strlen(arr[y - 1]))
+		{
+			//printf("up\n");
+			check_field(arr, x, y - 1);
+		}
+	}
+
+
+	// right
+	if (x + 1< ft_strlen(arr[y]))
+	{
+		if (y > 0)
+		{
+			//printf("right\n");
+			check_field(arr, x + 1, y);
+		}
+	}
+
+	// down
+	if (y + 1 < arr_len )
+	{
+		if (x < ft_strlen(arr[y + 1]))
+		{
+			//printf("down\n");
+			check_field(arr, x, y + 1);
+		}
+	}
+
+	// left
+	if (x - 1 < ft_strlen(arr[y]))
+	{
+		if (y > 0)
+		{
+			//printf("left\n");
+			check_field(arr, x - 1, y);
+		}
+	}
+
+	return (1);
+}
+
+
+int flood(char **arr)
+{
+	c_yellow(); printf("flood\n"); c_reset();
+	int c;
+	int r;
+	int row_count;
+	int col_count;
+
+	row_count = get_arr_len(arr);
+	r =0;
+	while(r < row_count)
+	{
+		c = 0;
+		col_count = ft_strlen(arr[r]);
+		while(c < col_count)
+		{
+			//printf("r: %i   c: %i \n", r, c);
+			if (arr[r][c] == 'F')
+				check_near_fields(arr, c, r);
+
+			c++;
+		}
+
+		r++;
+	}
+
+	return (1);
+}
+
 int check_map(t_map *s_map)
 {
 	char **f_map;
 
 	f_map = copy_arr(s_map->map);
-	c_yellow();
-	printf("check_map --> after copy map\n");
+	c_yellow(); printf("check_map --> after copy map\n"); c_reset();
 	set_first_f(f_map);
 	show_map(f_map);
+
+	int i = -1;
+	int arr_len = get_arr_len(f_map);
+	int max_col = get_max_line(f_map, 0, arr_len);
+	while (i++ < (arr_len * max_col) -428)
+		flood(f_map);
+		
 
 
 	return (1);
