a73f4b821217828d3201f5680f5b77c65f60d7ab
jonaspeters85 <jonaspeters8526@gmail.com>
2024-01-30 14:26:49 +0100

    check_map

--- src/loadmap/levelcheck.c
+++ src/loadmap/levelcheck.c
@@ -33,35 +33,22 @@ int check_field(char **arr, int x, int y)
 	if (x == 0 || x == col_len  || y == 0 || y == arr_len - 1)
 	{
 		if (arr[y][x] != '1')
-		{
-			printf("Error!\nspace in map or no wall\n");
-			return (0);
-		}
+			return (printf("Error!\nspace in map or no wall\n"), 0);
 	}
-
 	if (arr[y][x] == '0')
 	{
 		if (x == 0 || x == col_len - 1)
-		{
-			printf("ERROR!\nspace in map or no wall\n");
-			return (0);
-		}
+			return (printf("ERROR!\nspace in map or no wall\n"), 0);
 		if (y == 0 || y == arr_len - 1)
-		{	
-			printf("ERROR!\nspace in map or no wall\n");
-			return (0);
-		}
+			return (printf("ERROR!\nspace in map or no wall\n"), 0);
 
 		replace_char_at_pos(arr, y, x, 'F');
 		usleep(5000);
 		clear_screen();
 		show_map(arr);
 	}
-	else if (arr[y][x] == ' ')
-	{
-		printf("Error!\nspace in map or no wall\n");
-		return (0);
-	}
+	else if (arr[y][x] == ' ')	
+		return (printf("Error!\nspace in map or no wall\n"), 0);
 
 	return (1);
 }
@@ -140,14 +127,12 @@ int check_near_fields(char **arr, int x, int y)
 		return (0);
 	if (!check_left(arr, x, y))
 		return (0);
-
 	return (1);
 }
 
 
 int flood(char **arr)
 {
-	//c_yellow(); printf("flood\n"); c_reset();
 	int c;
 	int r;
 	int row_count;
@@ -161,7 +146,6 @@ int flood(char **arr)
 		col_count = ft_strlen(arr[r]);
 		while(c < col_count)
 		{
-			//printf("r: %i   c: %i \n", r, c);
 			if (arr[r][c] == 'F')
 			{
 				if (!check_near_fields(arr, c, r))
@@ -183,21 +167,21 @@ int check_map(t_map *s_map)
 {
 	char **f_map;
 	int ret;
+	int i;
+	int len;
 	t_xy tmp_pos;
 
 	f_map = copy_arr(s_map->map);
-	c_yellow(); printf("check_map --> after copy map\n"); c_reset();
 	set_first_f(f_map);
 
 
 	show_map(f_map);
 	tmp_pos = get_pos_of_char_in_arr(f_map, 'F');
 	ret = check_near_fields(f_map, tmp_pos.x, tmp_pos.y);
-	//printf("ret: %i\n", ret);
-	int i = -1;
-	int arr_len = get_arr_len(f_map);
-	int max_col = get_max_line(f_map, 0, arr_len);
-	while (i++ < (arr_len * max_col))
+
+	len =  get_arr_len(f_map) * get_max_line(f_map, 0, get_arr_len(f_map));
+	i = -1;
+	while (i++ < (len))
 	{
 		if (!flood(f_map))
 		{
