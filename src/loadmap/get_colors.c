6c6b3ba83572c059a9dbbdd0127ac1a74f6dbe96
jonaspeters85 <jonaspeters8526@gmail.com>
2024-02-01 16:14:55 +0100

    fixed get_color_from_str

--- src/loadmap/get_colors.c
+++ src/loadmap/get_colors.c
@@ -6,7 +6,7 @@
 /*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 14:04:16 by jopeters          #+#    #+#             */
-/*   Updated: 2024/02/01 15:57:59 by jonas            ###   ########.fr       */
+/*   Updated: 2024/02/01 16:14:13 by jonas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,14 +105,19 @@ t_color get_color_from_str(char **arr, char find)
 
     tmp_line = ft_strchr(arr[line],find);
     split = ft_split(++tmp_line, ',');
-    //show_arr(split);
-	if (check_color_section(split[0]) && check_line_for_chars(split[0], " 0123456789"))
-    	color.r = ft_atoi(split[0]);
-	if (check_color_section(split[1]) && check_line_for_chars(split[1], " 0123456789"))
-    	color.g = ft_atoi(split[1]);
-	if (check_color_section(split[2]) && check_line_for_chars(split[2], " 0123456789"))
-    	color.b = ft_atoi(split[2]);
-    color.a = 0;
+    show_arr(split);
+	if (get_arr_len(split) == 3)
+	{
+		if (check_color_section(split[0]) && check_line_for_chars(split[0], " 0123456789"))
+			color.r = ft_atoi(split[0]);
+		if (check_color_section(split[1]) && check_line_for_chars(split[1], " 0123456789"))
+			color.g = ft_atoi(split[1]);
+		if (check_color_section(split[2]) && check_line_for_chars(split[2], " 0123456789"))
+			color.b = ft_atoi(split[2]);
+		color.a = 0;
+	}
+	else
+		color = get_color(-1, -1, -1, -1);
 
 	//free(tmp_line);
 	//tmp_line = NULL;
