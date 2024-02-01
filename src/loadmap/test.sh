ecf552e0183890e33c4852167e80319203f8d599
jonaspeters85 <jonaspeters8526@gmail.com>
2024-02-01 15:40:42 +0100

    test.sh

--- src/loadmap/test.sh
+++ src/loadmap/test.sh
@@ -15,33 +15,33 @@ echo "===================="
 start=$1
 end=$2
 
-# make san
-# echo -e "${c_cyan}sanitize"
+make san
+echo -e "${c_cyan}sanitize"
 
-# sleep 3
-# for (( i=start; i<=end; i++ ))
-# do
-#    echo -e "${c_green}Map: ${c_yellow}m$i.cub${c_red}"
-#    sleep 1
-#    ./a.out m$i.cub
-#    echo ""
-#    sleep 0.1
-# done
+sleep 3
+for (( i=start; i<=end; i++ ))
+do
+   echo -e "${c_green}Map: ${c_yellow}m$i.cub${c_red}"
+   sleep 1
+   ./a.out m$i.cub
+   echo ""
+   sleep 0.1
+done
 
 
 
-# make
-# echo -e "${c_cyan}leaks"
+make
+echo -e "${c_cyan}leaks"
 
-# sleep 3
-# for (( i=start; i<=end; i++ ))
-# do
-#    echo -e "LEAKS ${c_green}Map: ${c_yellow}m$i.cub${c_red}"
-#    sleep 1
-#    leaks -atExit -- ./a.out m$i.cub
-#    echo ""
-#    sleep 2
-# done
+sleep 3
+for (( i=start; i<=end; i++ ))
+do
+   echo -e "LEAKS ${c_green}Map: ${c_yellow}m$i.cub${c_red}"
+   sleep 1
+   leaks -atExit -- ./a.out m$i.cub
+   echo ""
+   sleep 2
+done
 
 make san
 echo -e "${c_cyan}sanitize"
