97a06572776231243b047a89b929d3d43a0c0464
Jonas Peters <jopeters@c2s12d58.42wolfsburg.de>
2024-02-02 14:33:44 +0100

    ...

--- src/loadmap/get_colors.c
+++ src/loadmap/get_colors.c
@@ -6,7 +6,7 @@
 /*   By: jopeters <jopeters@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 14:04:16 by jopeters          #+#    #+#             */
-/*   Updated: 2024/02/02 14:27:40 by jopeters         ###   ########.fr       */
+/*   Updated: 2024/02/02 14:33:10 by jopeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int check_color(t_color color)
 }
 
 // checks if more elements than 1 are i a splittet , , section. 
-int check_color_section(char *str)
+int	check_color_section(char *str)
 {
 	char	**tmp;
 	int		arr_len;
@@ -43,7 +43,7 @@ int check_color_section(char *str)
 }
 
 // return 1 if only i.e ' ' are before char find
-int check_only_c_before_find(char *str, char c, char find)
+int	check_only_c_before_find(char *str, char c, char find)
 {
 	int	i;
 
@@ -57,6 +57,7 @@ int check_only_c_before_find(char *str, char c, char find)
 	return (1);
 }
 
+// return -1 if color not good
 t_color check_array_for_color(char *line, char find)
 {
 	char	**split;
@@ -64,9 +65,7 @@ t_color check_array_for_color(char *line, char find)
 	t_color	color;
 
     tmp_line = ft_strchr(line, find);
-
     split = ft_split(++tmp_line, ',');
-    //show_arr(split);
 	if (get_arr_len(split) == 3)
 	{
 		if (check_color_section(split[0]) && check_line_for_chars(split[0], " 0123456789"))
@@ -80,16 +79,15 @@ t_color check_array_for_color(char *line, char find)
 	else
 		color = get_color(-1, -1, -1, -1);
 	ft_free_array((void **)split);
-	//free(tmp_line);
 	return (color);
 }
 
 // returns the color of find ('C' or 'F') if not color is (-1, -1, -1, -1)
-t_color get_color_from_str(char **arr, char find)
+t_color		get_color_from_str(char **arr, char find)
 {
-    int line;
-    char str[2];
-	
+    int		line;
+    char	str[2];
+
     str[0] = find;
     str[1] = '\0';
 	if (get_line_of(arr, str, 0) == get_line_of(arr, str, 0))
@@ -98,24 +96,16 @@ t_color get_color_from_str(char **arr, char find)
 		return (get_color(-1, -1, -1, -1));
 	if (line == -1)
 		return (get_color(-1, -1, -1, -1));
-	
 	if (!check_only_c_before_find(arr[line], ' ', find))
 		return (get_color(-1, -1, -1, -1));
-	
-    // Check if line contains the find char ('F' or 'c') and 3 ','
     if ((count_char_in_str(arr[line], ',') != 2) || (count_char_in_str(arr[line], find) != 1))
         return (get_color(-1, -1, -1, -1));
-
-	// check for not allowed chars
 	if (!check_line_for_chars(arr[line], " ,FC0123456789"))
 		return (get_color(-1, -1, -1, -1));
-	
-	
-	
     return (check_array_for_color(arr[line], find));
 }
 
-void get_color_master(t_map *s_map, char **splitted_file)
+void	get_color_master(t_map *s_map, char **splitted_file)
 {
 	s_map->F = get_color_from_str(splitted_file, 'F');
 	s_map->C = get_color_from_str(splitted_file, 'C');
