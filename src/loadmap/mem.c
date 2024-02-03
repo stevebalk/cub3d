5801a410cf622cbd612b1eba4f964024fb37dae6
jonaspeters85 <jonaspeters8526@gmail.com>
2024-02-03 16:33:38 +0100

    norm mem.c

--- src/loadmap/mem.c
+++ src/loadmap/mem.c
@@ -3,30 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   mem.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
+/*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 15:15:01 by jopeters          #+#    #+#             */
-/*   Updated: 2024/02/02 13:45:10 by sbalk            ###   ########.fr       */
+/*   Updated: 2024/02/03 16:33:27 by jonas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "j_header.h"
 
-void free_s_map(t_map *s_map)
+void	free_s_map(t_map *s_map)
 {
-	//show_arr(s_map->textures);
-	
 	if (s_map->textures)
 		ft_free_array((void **)s_map->textures);
-
-	//free(s_map->textures);
 	if (s_map->map)
 		ft_free_array((void **)s_map->map);
-
-	// if (s_map->tex_names)
-	// {
-	// 	//printf("free text names\n");
-	// 	//free_n_null_2D((void ***)s_map->tex_names);
-	// 	ft_free_array((void **)s_map->tex_names);
-	// }
 }
