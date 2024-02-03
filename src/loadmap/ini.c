3835f9fbd01b43f0eab2c301db0e1cfd896e9dc3
jonaspeters85 <jonaspeters8526@gmail.com>
2024-02-03 15:57:06 +0100

    norm ini.c

--- src/loadmap/ini.c
+++ src/loadmap/ini.c
@@ -1,21 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ini.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/03 15:56:44 by jonas             #+#    #+#             */
+/*   Updated: 2024/02/03 15:56:56 by jonas            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "j_header.h"
 
-// inizialize tex short names
-// char **ini_tex_names(void)
-// {
-// 	char **ret;
-// 	ret = (char **)malloc((sizeof(char *) * (5)));
-// 	ret[0] = ft_strdup("NO");
-// 	ret[1] = ft_strdup("EA");
-// 	ret[2] = ft_strdup("SO");
-// 	ret[3] = ft_strdup("WE");
-// 	ret[4] = NULL;
-// 	//show_arr(ret);
-// 	return (ret);
-// }
-
 // set all vars to -1
-void ini_player_pos(t_map *s_map)
+void	ini_player_pos(t_map *s_map)
 {
 	s_map->player_start_pos.view = -1;
 	s_map->player_start_pos.x = -1;
