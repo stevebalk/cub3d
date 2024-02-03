1f1aa054c9c1bcfc1e1826fb9a913e83cfcd5046
jonaspeters85 <jonaspeters8526@gmail.com>
2024-02-03 17:48:09 +0100

    added new test2.sh script for maps in testmaps

---
+++ src/loadmap/test2.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+c_purple="\033[0;35m"
+c_red="\033[0;31m"
+c_green="\033[0;32m"
+c_blue="\033[0;34m"
+c_yellow="\033[0;33m"
+c_cyan="\033[0;36m"
+c_reset="\033[0m"
+
+echo ""
+echo -e "${c_cyan}Map Tester 2001${c_purple}  ${1}  ${2}"
+echo "============================"
+
+# Check if at least one argument is given
+if [ $# -lt 2 ]; then
+  echo "Folder: $0 <directory_path>"
+  echo "Test >> $1"
+  exit 1
+else
+	echo "First argument has to be the folder of files, 2nd the Test SAN or LEAKS"
+fi
+
+# Check if the argument is a directory
+if [ ! -d "$1" ]; then
+  echo "The provided argument is not a valid directory."
+  exit 2
+fi
+
+directory=$1
+string_to_check=$2
+
+if [ "$string_to_check" == "SAN" ]; then
+	make san
+fi
+
+if [ "$string_to_check" == "LEAKS" ]; then
+	echo "LEAKS"
+	make
+fi
+
+# Loop through the files in the directory
+for file in "$directory"/*
+do
+  if [ -f "$file" ]; then
+		if [ "$string_to_check" == "LEAKS" ]; then
+
+    	echo -e "${c_purple}LEAKS ${c_green}Map: $file${c_reset}"
+    	#echo -e "${c_purple}file: $file${c_reset}"
+		sleep 0.2
+		leaks -atExit -- ./a.out $file
+		echo -e "${c_reset}"
+		sleep 1
+
+		fi
+
+		if [ "$string_to_check" == "SAN" ]; then
+
+    	echo -e "${c_purple}SAN ${c_green}Map: $file${c_reset}"
+    	#echo -e "${c_purple}file: $file${c_reset}"
+		sleep 0.2
+		./a.out $file
+		echo -e "${c_reset}"
+		sleep 1
+
+		fi
+  fi
+done
