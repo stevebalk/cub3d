dc205dffac329b815579601e4e073a752bc7b749
Jonas Peters <jopeters@c2s16d99.42wolfsburg.de>
2024-02-05 13:16:48 +0100

    get_map_lines_master() new because of 25 line rule

--- src/loadmap/check_map.c
+++ src/loadmap/check_map.c
@@ -6,7 +6,7 @@
 /*   By: jopeters <jopeters@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 14:05:59 by jopeters          #+#    #+#             */
-/*   Updated: 2024/02/05 12:12:32 by jopeters         ###   ########.fr       */
+/*   Updated: 2024/02/05 13:14:43 by jopeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,23 @@ int	check_map_valid(t_map *s_map)
 	return (1);
 }
 
-// return 0 if there are lines other than textures, color or map
-int	check_for_bad_lines(char **arr)
+t_map_lines	get_map_lines_master(char **arr)
 {
-	int			i;
 	t_map_lines	map_lines;
 
 	if (BONUS_ == 0)
 		map_lines = get_map_lines(arr, " 01NESW", "01NESW");
 	else if (BONUS_ == 1)
 		map_lines = get_map_lines(arr, " 012345NESW", "012345NESW");
+}
+
+// return 0 if there are lines other than textures, color or map
+int	check_for_bad_lines(char **arr)
+{
+	int			i;
+	t_map_lines	map_lines;
+
+	map_lines = get_map_lines_master(arr);
 	i = -1;
 	while (++i < map_lines.start)
 	{
