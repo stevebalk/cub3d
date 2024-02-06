aab06764c471a616c4501cbdedc0904ae4de17be
Jonas Peters <jopeters@c2s7d23.42wolfsburg.de>
2024-02-06 11:27:05 +0100

    moved testfiles for loadmap to testmap folder

--- src/loadmap/test/test.c
+++ src/loadmap/test/test.c
@@ -1,35 +1,28 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 
 void show_iarr(int **arr)
 {
-	printf("show\n");
 	int i = -1;
-
+	printf("show\n");
 	while(arr[++i])
 		printf("i: %i   = %i\n", i, arr[i][0]);
 }
 
-// int main(void)
-// {
-// 	int count = 10;
-// 	int i = 0;
-// 	int **iarr;
+int main(void)
+{
+	int count = 10;
+	int i = 0;
+	int **iarr;
 
-// 	iarr = (int **)malloc(sizeof(int *)*count);
-
-// 	iarr[9] = NULL;
-
-// 	while (i < 9)
-// 	{
-// 		iarr[i] = malloc(sizeof(int));
-// 		iarr[i][0] = i;
-// 		i++;
-// 	}
-	
-// 	show_iarr(iarr);
-
-
-// 	return 0;
-// }
+	iarr = (int **)malloc(sizeof(int *)*count);
+	iarr[9] = NULL;
+	while (i < 9)
+	{
+		iarr[i] = malloc(sizeof(int));
+		iarr[i][0] = i;
+		i++;
+	}
+	show_iarr(iarr);
+	return 0;
+}
