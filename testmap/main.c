c6c060c070e3e43a7d83f7197becf056de918173
Jonas Peters <jopeters@c2s7d23.42wolfsburg.de>
2024-02-06 11:35:59 +0100

    test makefile

---
+++ testmap/main.c
@@ -0,0 +1,20 @@
+#include "j_header.h"
+
+int main(int argc, char **argv)
+{   
+	t_map map;
+
+	int check_res;
+	check_res = load_and_check(&map, argc, argv);
+	if (check_res)
+		printf("run GAME \n");
+	free_s_map(&map);
+	c_green();
+	printf("-- programm END --\n\n");
+	c_reset();
+	if (check_res == 0)
+		check_res++;
+	if (check_res == 1)
+		check_res--;
+    return(check_res);
+}
