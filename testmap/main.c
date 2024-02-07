1639851e70987b6e05e3fb25036d96ab10cb857b
Jonas Peters <jopeters@c2r11s3.42wolfsburg.de>
2024-02-07 16:38:12 +0100

    testmap/main.c norm ok

--- testmap/main.c
+++ testmap/main.c
@@ -1,17 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jopeters <jopeters@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 16:37:32 by jopeters          #+#    #+#             */
+/*   Updated: 2024/02/07 16:37:54 by jopeters         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "j_header.h"
 
-int main(int argc, char **argv)
-{   
-	t_map map;
-	#ifdef BONUS
-		printf(" --- Check BONUS ---\n");
-		usleep(500000);
-	#else
-		printf(" --- Check ---\n");
-		usleep(500000);
-	#endif
+int	main(int argc, char **argv)
+{
+	t_map	map;
+	int		check_res;
 
-	int check_res;
 	check_res = load_and_check(&map, argc, argv);
 	if (check_res)
 		printf("run GAME \n");
@@ -23,5 +28,5 @@ int main(int argc, char **argv)
 		check_res++;
 	if (check_res == 1)
 		check_res--;
-    return(check_res);
+	return (check_res);
 }
